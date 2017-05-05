7c37b3a12b07f04be4309bca67fb0b197cc801a9
Lin <WYexe@hotmail.com>
2017-05-05 19:28:34 +0800

    Feature Injector

--- GameDLL/dllmain.cpp
+++ GameDLL/dllmain.cpp
@@ -1,7 +1,6 @@
 // dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "stdafx.h"
 #include <MyTools/Log.h>
-#include <MyTools/CLNoModuleDLL.h>
 #include <MyTools/Character.h>
 
 #define _SELF L"dllmain.cpp"
@@ -19,20 +18,15 @@ DWORD WINAPI _WorkThread(LPVOID)
 		LOG_C_D(L"Running");
 		::Sleep(5 * 1000);
 	}
-	return 0;
+
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule,DWORD  ul_reason_for_call,LPVOID lpReserved)
+BOOL APIENTRY DllMain(HMODULE ,DWORD  ,LPVOID )
 {
-	//CLNoModuleDLL NoModuleDLL;
-	//NoModuleDLL.HideSelf(hModule, ul_reason_for_call, lpReserved);
-	if (hWorkThread == NULL /*&& ul_reason_for_call == NoModuleDLL.GetNoModuleReasonValue()*/)
+	if (hWorkThread == NULL)
 	{
 		hWorkThread = cbBEGINTHREADEX(NULL, NULL, _WorkThread, NULL, NULL, NULL);
 	}
-	//else if (ul_reason_for_call == DLL_PROCESS_ATTACH)
-	//	return FALSE;
-
 	return TRUE;
 }
 
--- InjectorDLL/dllmain.cpp
+++ InjectorDLL/dllmain.cpp
@@ -5,23 +5,28 @@
 #include <MyTools/CLHook.h>
 #include <MyTools/CLNoModuleDLL.h>
 #include <MyTools/CLProcess.h>
-#include "LdrHeader.h"
-
+#include <MyTools/CLFile.h>
 #define _SELF L"dllmain.cpp"
-
-BOOL LdrLoadMemDll(PVOID DllMem, SIZE_T dllSize, LPCWSTR szDllName, HMODULE *pModule);
 
 VOID LoadGameDLL(_In_ CONST LPPROCESS_INFORMATION lpProcessInformation)
 {
-	LOG_C_D(L"lpProcessInformation=%X", lpProcessInformation);
 	if (lpProcessInformation != nullptr)
 	{
-		CLProcess::LoadRemoteDLL(lpProcessInformation->dwProcessId, L"C:\\Visual Studio 2013\\Projects\\DHSH\\Release\\InjectorDLL.dll");
-		ResumeThread(lpProcessInformation->hThread);
+		LPVOID lpFileContent = nullptr;
+		UINT uSize = 0;
+		if (CLFile::ReadFileContent(L"C:\\Share\\InjectorDLL.dll", lpFileContent, uSize))
+		{
+			CLNoModuleDLL NoModuleDLL;
+			NoModuleDLL.RemoteLoadMemoryDLL(lpFileContent, uSize, lpProcessInformation->hProcess, lpProcessInformation->hThread);
+			ResumeThread(lpProcessInformation->hThread);
+			::VirtualFree(lpFileContent, 0, MEM_RELEASE);
+		}
 	}
 
 }
 
+#pragma warning(push)
+#pragma warning(disable:4740)
 DWORD dwHookAddr = 0;
 __declspec(naked) void HookCreateProcessA()
 {
@@ -81,40 +86,35 @@ __declspec(naked) void HookCreateProcessA()
 			MOV lpProcessInformation, EAX;
 
 		}
-
+		__asm PUSHAD;
 		if (CCharacter::strstr_my(lpCommandLine, "zy@fs"))
 		{
 			bHook = TRUE;
 			//CLProcess::CreateProcess_Injector_DLL(lpCommandLine, L"", lpProcessInformation);
-			::CreateProcessA(lpApplicationName, lpCommandLine, lpProcessAttributes, lpThreadAttributes, bInheritHandles, dwCreationFlags/* | CREATE_SUSPENDED*/, lpEnvironment, lpCurrentDirectory, lpStartupInfo, lpProcessInformation);
+			::CreateProcessA(lpApplicationName, lpCommandLine, lpProcessAttributes, lpThreadAttributes, bInheritHandles, dwCreationFlags | CREATE_SUSPENDED, lpEnvironment, lpCurrentDirectory, lpStartupInfo, lpProcessInformation);
 			LoadGameDLL(lpProcessInformation);
 			bHook = FALSE;
+			__asm POPAD;
 			__asm RETN 0x28;
 		}
 		else
 		{
-			__asm
-			{
-				__asm PUSH EBP;
-				__asm MOV EBP, ESP;
-				__asm MOV EAX, dwHookAddr;
-				__asm ADD EAX, 0x5;
-				__asm JMP EAX;
-			}
+			__asm POPAD;
+			__asm PUSH EBP;
+			__asm MOV EBP, ESP;
+			__asm MOV EAX, dwHookAddr;
+			__asm ADD EAX, 0x5;
+			__asm JMP EAX;
 		}
-
-
 	}
-
 }
-
+#pragma warning(pop)
 
 HANDLE hWorkThread = NULL;
-DWORD WINAPI _WorkThread(LPVOID lpParm)
+DWORD WINAPI _WorkThread(LPVOID)
 {
 	WCHAR wszPath[MAX_PATH] = { 0 };
 	::GetModuleFileNameW(NULL, wszPath, MAX_PATH);
-
 	std::wstring wsPath = CCharacter::MakeTextToLower(wszPath);
 	if (wsPath.find(L"cprotect1.exe") != -1)
 	{
@@ -124,61 +124,27 @@ DWORD WINAPI _WorkThread(LPVOID lpParm)
 		HookContent.uNopCount = 0x0;
 		CLHook::Hook_Fun_Jmp_MyAddr(&HookContent);
 	}
-	else if (wsPath.find(L"game.exe") != -1)
+	else if(wsPath.find(L"game.exe") != -1)
 	{
-		//CLog::GetInstance().SetClientName(L"InjectorClient", wszPath, TRUE, 20 * 1024 * 1024);
-		std::wstring _wsModuleName = L"C:\\Visual Studio 2013\\Projects\\DHSH\\Release\\GameDLL.dll";
-		HANDLE hFile = CreateFileW(_wsModuleName.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-		if (INVALID_HANDLE_VALUE == hFile)
+		LPVOID lpFileContent = nullptr;
+		UINT uSize = 0;
+		if (CLFile::ReadFileContent(L"C:\\Share\\GameDLL.dll", lpFileContent, uSize))
 		{
-			//LOG_CF(CLog::em_Log_Type::em_Log_Type_Exception, L"hFile = NULL, strFileName = %s", _wsModuleName.c_str());
-			return FALSE;
+			CLNoModuleDLL NoModuleDLL;
+			BOOL bRet = NoModuleDLL.LoadMemoryDLL(lpFileContent, uSize);
+			::VirtualFree(lpFileContent, 0, MEM_RELEASE);
 		}
-
-		DWORD _dwMemSize = GetFileSize(hFile, NULL);
-		if (_dwMemSize == 0)
-		{
-			//LOG_CF(CLog::em_Log_Type::em_Log_Type_Exception, L"nFileSize = 0");
-			return FALSE;
-		}
-		LPVOID _pRelocMemPtr = VirtualAlloc(NULL, _dwMemSize, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-		if (_pRelocMemPtr == nullptr)
-		{
-			//LOG_CF(CLog::em_Log_Type::em_Log_Type_Exception, L"VirtualAlloc = 0");
-			return FALSE;
-		}
-
-		DWORD dwReadSize = 0;
-		if (!ReadFile(hFile, _pRelocMemPtr, _dwMemSize, &dwReadSize, NULL))
-		{
-			//LOG_CF(CLog::em_Log_Type::em_Log_Type_Exception, L"ReadFile = 0");
-			return FALSE;
-		}
-
-		TCHAR szSysDll[MAX_PATH] = { 0 };
-		TCHAR szDllName[] = { '\\', 'a', 's', 'y', 'c', 'f', 'i', 'l', 't', '.', 'd', 'l', 'l', 0 };
-		GetSystemDirectory(szSysDll, MAX_PATH);
-		lstrcat(szSysDll, szDllName);
-		HMODULE hmDLL = NULL;
-		LdrLoadMemDll(_pRelocMemPtr, _dwMemSize, szSysDll, &hmDLL);
 	}
-
 	return 0;
 }
 
-BOOL APIENTRY DllMain(HMODULE hModule,
-	DWORD  ul_reason_for_call,
-	LPVOID lpReserved
-	)
+BOOL APIENTRY DllMain(HMODULE ,DWORD  ,LPVOID )
 {
-	CLNoModuleDLL NoModuleDLL;
-	NoModuleDLL.HideSelf(hModule, ul_reason_for_call, lpReserved);
-	if (NoModuleDLL.GetNoModuleReasonValue() == ul_reason_for_call && hWorkThread == NULL)
+	if (hWorkThread == NULL)
 	{
-		hWorkThread = cbBEGINTHREADEX(NULL, NULL, _WorkThread, &hModule, NULL, NULL);
+		hWorkThread = cbBEGINTHREADEX(NULL, NULL, _WorkThread, NULL, NULL, NULL);
 		::CloseHandle(hWorkThread);
 	}
-
-	return ul_reason_for_call == DLL_PROCESS_ATTACH ? FALSE : TRUE;
+	return TRUE;
 }
 
